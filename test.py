80ab46fc42c69b0e4c74875171d32b834919044e
Prabal Jain <65130817+prabalJain1998@users.noreply.github.com>
2022-02-21 11:34:44 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x/y;
+
+
+print(add(2,3))
+print(subtract(5,6))
+print(multiply(2,4))
+print(divide(10,4))
